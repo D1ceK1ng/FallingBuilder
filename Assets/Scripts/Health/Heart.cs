9747e294b441901560590d545d409b52e0654f78
Danil Tkachov <gamesdanik4@gmail.com>
2022-10-24 19:46:55 +0300

    UI improvements

---
+++ Assets/Scripts/Health/Heart.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class Heart : MonoBehaviour
+{
+    [SerializeField] private Image _icon;
+   public void TakeDamage()
+   {
+    _icon.color = Color.black;
+   }
+}
